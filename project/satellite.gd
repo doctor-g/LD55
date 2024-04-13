56fa3e2a75b7088f597ebe998560a7745f138e76
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-13 15:12:14 -0400

    Spread out orbitals a bit more
    This makes the first move a bit less difficult

--- project/satellite.gd
+++ project/satellite.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 const SPEED := 5.0
 const ACCELERATION := 0.1
+const DISTANCE_PER_ORBITAL := 1.2
 
 var angle := 0.0
 var orbital := 1
@@ -11,7 +12,7 @@ var _radius := 0.0
 
 func _physics_process(delta: float) -> void:
 	angle = fmod(angle + SPEED / orbital * delta, TAU)
-	_radius = move_toward(_radius, orbital, ACCELERATION)
+	_radius = move_toward(_radius, orbital * DISTANCE_PER_ORBITAL, ACCELERATION)
 	position = Vector3(sin(angle), 0, cos(angle)) * _radius
 
 
