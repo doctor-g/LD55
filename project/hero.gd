dde0ae59fcd3d592a82be1dcee53f2fe032dc064
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-13 11:14:17 -0400

    Show and use charges

--- project/hero.gd
+++ project/hero.gd
@@ -4,7 +4,17 @@ const SPEED := 20.0
 const ACCELERATION := 1.0
 const SATELLITE_SCENE := preload("res://satellite.tscn")
 
+var charges := 1:
+	set(value):
+		charges = value
+		_update_charges_label()
+
+
 @onready var _satellites := $Satellites
+
+func _ready() -> void:
+	_update_charges_label()
+
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_down", "move_up")
@@ -15,8 +25,9 @@ func _physics_process(_delta: float) -> void:
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("summon"):
-		# Move all the other ones out a unit
+	if Input.is_action_just_pressed("summon") and charges > 0:
+		charges -= 1
+		
 		for satellite in _satellites.get_children():
 			satellite.orbital += 1
 		
@@ -28,8 +39,12 @@ func _physics_process(_delta: float) -> void:
 
 
 func charge() -> void:
-	print("Charging up!")
+	charges += 1
 
 
 func damage() -> void:
 	print("OUCH")
+
+
+func _update_charges_label() -> void:
+	%ChargesLabel.text = "Charges: %d" % charges
