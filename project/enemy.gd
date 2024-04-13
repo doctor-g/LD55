ba3ee54b82b9a8f85d9183e9e339a500c0a5837b
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-13 08:59:45 -0400

    Improve feel by considering acceleration

--- project/enemy.gd
+++ project/enemy.gd
@@ -1,13 +1,19 @@
 extends CharacterBody3D
 
+const ACCELERATION := 3.0
+
 @export var speed := 7.0
 @export var target : Node3D
 
+var _direction := Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	if target != null:
-		var direction := (target.global_position - global_position).normalized()
-		var collision := move_and_collide(direction * speed * delta)
+		var new_direction := (target.global_position - global_position).normalized()
+		
+		_direction = _direction.move_toward(new_direction, ACCELERATION * delta)
+		
+		var collision := move_and_collide(_direction * speed * delta)
 		if collision != null and collision.get_collider() == target:
 			target.damage()
 			queue_free()
--- project/hero.gd
+++ project/hero.gd
@@ -1,12 +1,19 @@
 extends CharacterBody3D
 
-const SPEED := 25.0
+const SPEED := 20.0
+const ACCELERATION := 5.0
 const SATELLITE_SCENE := preload("res://satellite.tscn")
+
+var _velocity := Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_down", "move_up")
 	var vector := Vector3(direction.x, 0, -direction.y).normalized()
-	move_and_collide(vector * SPEED * delta)
+	var target_velocity := vector * SPEED * delta
+	
+	_velocity = _velocity.move_toward(target_velocity, ACCELERATION * delta)
+	
+	move_and_collide(_velocity)
 	
 	if Input.is_action_just_pressed("summon"):
 		# Summon a pair for fun
