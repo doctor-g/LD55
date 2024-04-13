263a133cd0921bff8b10d608411856db4398cecb
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-13 17:06:28 -0400

    Use keyboard to start
    This prevents awkward movement from mouse to keys.

--- project/title_screen.gd
+++ project/title_screen.gd
@@ -2,14 +2,21 @@ extends Node3D
 
 static var _use_splash := true
 
+var _ready_to_start := false
+
 func _ready() -> void:
 	if not _use_splash:
 		$Splash.queue_free()
 		_show_main_ui()
 
 
-func _on_play_button_pressed() -> void:
-	%PlayButton.disabled = true
+func _input(event: InputEvent) -> void:
+	if _ready_to_start and event is InputEventKey and event.keycode == KEY_SPACE:
+		_ready_to_start = false
+		_start_game()
+
+
+func _start_game() -> void:
 	$AnimationPlayer.play("fade_out")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://world.tscn")
@@ -20,7 +27,6 @@ func _on_label_meta_clicked(meta: Variant) -> void:
 
 
 func _on_splash_splash_dismissed() -> void:
-	$Music.play()
 	await create_tween().tween_property($Splash, "modulate:a", 0, 0.5).finished
 	$Splash.queue_free()
 	_show_main_ui()
@@ -28,6 +34,8 @@ func _on_splash_splash_dismissed() -> void:
 
 
 func _show_main_ui() -> void:
+	$Music.play()
 	$MainUI.visible = true
 	$MainUI.modulate.a = 0
-	create_tween().tween_property($MainUI, "modulate:a", 1, 0.5)
+	await create_tween().tween_property($MainUI, "modulate:a", 1, 0.5).finished
+	_ready_to_start = true
