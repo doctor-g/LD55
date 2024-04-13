865ac3e1eb62c968cd70348fa2e0079e7b3ba5fd
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-13 10:36:54 -0400

    Add walls around the world

--- project/hero.gd
+++ project/hero.gd
@@ -1,19 +1,17 @@
 extends CharacterBody3D
 
 const SPEED := 20.0
-const ACCELERATION := 5.0
+const ACCELERATION := 1.0
 const SATELLITE_SCENE := preload("res://satellite.tscn")
 
-var _velocity := Vector3.ZERO
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_down", "move_up")
 	var vector := Vector3(direction.x, 0, -direction.y).normalized()
-	var target_velocity := vector * SPEED * delta
+	var target_velocity := vector * SPEED
 	
-	_velocity = _velocity.move_toward(target_velocity, ACCELERATION * delta)
+	velocity = velocity.move_toward(target_velocity, ACCELERATION)
 	
-	move_and_collide(_velocity)
+	move_and_slide()
 	
 	if Input.is_action_just_pressed("summon"):
 		# Summon a pair for fun
